a45b51867e0d18bbcf7ebfadb452e3b7dd4e5013
Matheus Gomes da Silva <matheus.silva@telecall.com>
2022-10-19 14:59:22 -0300

    Programa finalizado

---
+++ reading-files-with-linq/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reading_files_with_linq.Entities
+{
+    class Product
+    {
+        public Product(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Preco}";
+        }
+    }
+}
--- reading-files-with-linq/Program.cs
+++ reading-files-with-linq/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using reading_files_with_linq.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace reading_files_with_linq
 {
@@ -6,7 +11,26 @@ namespace reading_files_with_linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string diretorio = @"C:\Users\matheus.silva\Documents\ProjetosPessoais\reading-files-with-linq\reading-files-with-linq\Produtos.csv";
+
+            string[] linhas = File.ReadAllLines(diretorio);
+            List<Product> produtos = new List<Product>();
+            foreach (string linha in linhas)
+            {
+                string[] linhaDividida = linha.Split(",");
+                string nome = linhaDividida[0];
+                double preco = double.Parse((linhaDividida[1]), CultureInfo.InvariantCulture);
+                produtos.Add(new Product(nome, preco));
+            }
+
+            var q1 = produtos.Average(p => p.Preco);
+            Console.Write("Média de valores de produtos: ");
+            Console.WriteLine(q1.ToString("F2", CultureInfo.InvariantCulture));
+
+            var q2 = produtos.Where (p => p.Preco < q1).OrderByDescending(p => p.Nome).ToList();
+
+            q2.ForEach(p => Console.WriteLine(p.Nome));
         }
     }
 }
