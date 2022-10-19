19a5e94420944d0252c03fa292888b712f8aa2d9
Matheus Gomes da Silva <matheus.silva@telecall.com>
2022-10-19 13:51:01 -0300

    Commit inicial

---
+++ reading-files-with-linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reading_files_with_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
